217f0abd4f416cb81dff4fe4770bcb58a8c42710
Robert Wolff <robert.wolff@idealo.de>
2022-08-19 13:46:01 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { RadioButton } from '@idealo/ids-react'
+import styled from 'styled-components'
+
+const Wrapper = styled.p`
+  display: flex;
+  flex-direction: column;
+
+  & > div {
+    margin-bottom: 10px;
+  }
+`
+
+type ButtonGroup = {
+  [key: string]: boolean
+}
+
+const buttonGroup: ButtonGroup = {
+  red: false,
+  yellow: false,
+  green: false
+}
 
 function App() {
+  const [select, setSelected] = useState(buttonGroup)
+
+  const handleChange = (buttonName: string) => {
+    setSelected({ ...buttonGroup, [buttonName]: true })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Wrapper>
+          {Object.keys(buttonGroup).map((buttonName) => (
+            <RadioButton
+              name={buttonName}
+              id={buttonName}
+              key={buttonName}
+              checked={select[buttonName]}
+              onChange={() => handleChange(buttonName)}
+              label={buttonName}
+            />
+          ))}
+        </Wrapper>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
